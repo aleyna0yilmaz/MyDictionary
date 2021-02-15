c056c4ec8cbed24bdfbc460e8e732a17654221e6
aleyna <aleyna@DESKTOP-02M5UM7>
2021-02-15 15:01:28 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey,TValue>
+    {
+        Tkey[] itemKey;
+        TValue[] itemValue;
+
+        public MyDictionary()
+        {
+            itemKey = new Tkey[0];
+            itemValue = new TValue[0];
+        }
+        public void Add(Tkey key, TValue value)
+        {
+            Tkey[] tempArrayKey = itemKey;
+            TValue[] tempArrayValue = itemValue;
+
+
+            itemKey = new Tkey[itemKey.Length + 1];
+            itemValue = new TValue[itemValue.Length + 1];
+
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                itemKey[i] = tempArrayKey[i];
+                itemValue[i] = tempArrayValue[i];
+
+            }
+
+
+            itemKey[itemKey.Length - 1] = key;
+            itemValue[itemValue.Length - 1] = value;
+
+        }
+
+
+
+        public int Count
+        {
+            get { return itemKey.Length; }
+        }
+
+
+
+        public Tkey[] KeyList
+        {
+            get { return itemKey; }
+
+        }
+
+        public TValue[] ValueList
+        {
+            get { return itemValue; }
+
+        }
+    }
+    
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+           
+            Dictionary<int,string> product = new Dictionary<int,string>();
+            Console.WriteLine("product.Count : " + product.Count);
+
+            product.Add(1, "kalem a");
+            product.Add(2, "kitap b");
+            product.Add(3, "masa c");
+
+            foreach (var products in product)
+            {
+                Console.WriteLine(products);
+
+            }
+
+            Console.WriteLine("products.Count : " + product.Count);
+
+
+            Console.WriteLine("--------------------");
+
+            MyDictionary<int, string> myProducts = new MyDictionary<int, string>();
+            Console.WriteLine("myProducts.Count : " + myProducts.Count);
+
+            myProducts.Add(1, "kitap a");
+            myProducts.Add(2, "kitap b");
+            myProducts.Add(3, "kitap c");
+
+            foreach (var products in myProducts.KeyList)
+            {
+                Console.WriteLine(product);
+
+            }
+
+            foreach (var products in myProducts.ValueList)
+            {
+                Console.WriteLine(product);
+
+            }
+
+
+
+            Console.WriteLine("myProducts.Count : " + myProducts.Count);
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
